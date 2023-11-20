e2975d4ba884eeccb51c60a07995702f46850b9f
aileen-18 <nargizamatem@gmail.com>
2023-11-20 06:46:39 +0500

    second commit

--- app/src/main/java/uz/itschool/mathgame/screen/HomeScreen.kt
+++ app/src/main/java/uz/itschool/mathgame/screen/HomeScreen.kt
@@ -1,26 +1,36 @@
 package uz.itschool.mathgame.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableIntState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,165 +38,134 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import uz.itschool.mathgame.R
 import uz.itschool.mathgame.navigation.NavGraph
 import java.util.Random
 
 @Composable
 fun HomeScreen(navController: NavController) {
+    val score = remember { mutableIntStateOf(0) }
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = Color.White
     ) {
-        var score by remember { mutableStateOf(0) }
-        var questionIndex by remember { mutableStateOf(1) }
-
-        var correctAnswer by remember { mutableStateOf("") }
-        var answersForDisplay by remember { mutableStateOf<List<String>>(emptyList()) }
-        var onAnswerSelected: (String) -> Unit = { userAnswer ->
-            // Your logic here
-            println("User selected: $userAnswer")
-        }
-
-        LaunchedEffect(true) {
-            // Generate a random math problem with correct answer
-            val (correctProblem, problemsForDisplay) = generateMathProblemWithAnswer()
-
-            // Initialize onAnswerSelected lambda function
-            onAnswerSelected = { userAnswer ->
-                // Check the answer and update the score
-                val isCorrect = userAnswer == correctProblem
-                if (isCorrect) {
-                    // Update the score
-                    // You may want to use a ViewModel or other architecture for more complex apps
-                    // This is a simplified example
-                    score++
-                }
-                // Move to the next question or finish the game
-                questionIndex++
-                if (questionIndex > 10) {
-                    // Navigate to ScoreScreen or perform any other action
-                    // You may want to use navigation components for a more robust solution
-                    // This is a simplified example
-                }
-            }
-        }
-
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(16.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+                .background(Color(0xFF222853)),
         ) {
-            // Display the problems for the player to choose
-            Text(
-                text = problemsForDisplay,
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(16.dp)
-            )
-
-            // Display buttons for operations (+, -, *, /)
-            Row(
+            // App bar
+            Box(
+                contentAlignment = Alignment.CenterStart,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(16.dp),
-                horizontalArrangement = Arrangement.SpaceBetween
+                    .background(color = colorResource(id = com.airbnb.lottie.R.color.material_blue_grey_800)),
             ) {
-                // Button for each problem (user's choices)
-                MathOperationButton(correctProblem, onAnswerSelected)
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(0.dp, 4.dp)
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .size(45.dp)
+                            .clip(shape = RoundedCornerShape(18.dp))
+                            .background(Color.White),
+                        contentAlignment = Alignment.Center
+                    ) {
+
+                    }
+                }
+            }
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f, true)
+            )
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(12.dp)
+                    .background(
+                        color = colorResource(id = com.airbnb.lottie.R.color.material_blue_grey_800),
+                        shape = RoundedCornerShape(64.dp)
+                    ),
+
+                ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                ) {
+
+                    Row(modifier = Modifier.padding(0.dp, 6.dp, 0.dp, 0.dp)) {
+                        SymbolButton(0, R.drawable.minus_icon, score)
+                        SymbolButton(1, R.drawable.plus_icon, score)
+                    }
+                    Row {
+                        SymbolButton(2, R.drawable.divide_icon, score)
+                        SymbolButton(3, R.drawable.multiply_icon, score)
+                    }
+                }
+            }
+
+
+        }
+
+
+    }
+}
+@Composable
+fun RowScope.SymbolButton(id: Int, imageResource: Int, scoreVar: MutableIntState) {
+    Button(
+        modifier = Modifier
+            .weight(1f, true)
+            .aspectRatio(1f, true)
+            .padding(8.dp),
+        onClick = {
+            onSymbolClick(id, scoreVar)
+        },
+        shape = RoundedCornerShape(64.dp),
+        colors = ButtonDefaults.buttonColors(containerColor = Color.White)
+
+    ) {
+        Image(painter = painterResource(id = imageResource), contentDescription = "minus icon")
+    }
+}
+fun onSymbolClick(id: Int, scoreVar: MutableIntState) {
+    scoreVar.intValue += 1
+}
+        fun checkAnswer(problem: String, userAnswer: String): Boolean {
+            try {
+                // Split the problem into operands and operator
+                val (operand1, operator, operand2) = problem.split(" ")
+
+                // Convert operands to integers
+                val num1 = operand1.toInt()
+                val num2 = operand2.toInt()
+
+                // Evaluate the result based on the operator
+                val expectedResult = when (operator) {
+                    "+" -> num1 + num2
+                    "-" -> num1 - num2
+                    "*" -> num1 * num2
+                    "/" -> num1 / num2
+                    else -> throw IllegalArgumentException("Invalid operator: $operator")
+                }
+
+                // Check if the user's answer is correct
+                return userAnswer.toIntOrNull() == expectedResult
+            } catch (e: Exception) {
+                // Handle any parsing or arithmetic exceptions
+                e.printStackTrace()
+                return false
             }
         }
 
-    }
-}
-    @Composable
-    fun MathOperationButton(
-        symbol: String,
-        onAnswerSelected: (String) -> Unit
-    ) {
-        Button(
-            onClick = { onAnswerSelected(symbol) },
-            modifier = Modifier
-                .size(80.dp) // Adjust the button size as needed
-                .padding(8.dp)
-
-
-        ) {
-            Text(symbol,
-                fontSize = 20.sp)
-        }
-    }
-
-fun generateMathProblemWithAnswer(): Pair<String, String>{
-    val random = Random(System.currentTimeMillis())
-
-    // Generate two random operands
-    val operand1 = random.nextInt(10) // Adjust the range as needed
-    val operand2 = random.nextInt(10) // Adjust the range as needed
-
-    // Randomly select an operator
-    val operators = listOf("+", "-", "*", "/")
-    val operator = operators[random.nextInt(operators.size)]
-
-    // Evaluate the result based on the operator
-    val result = when (operator) {
-        "+" -> operand1 + operand2
-        "-" -> operand1 - operand2
-        "*" -> operand1 * operand2
-        "/" -> operand1 / operand2
-        else -> throw IllegalArgumentException("Invalid operator: $operator")
-    }
-
-    // Create a problem with the correct answer
-    val problemWithAnswer = "$operand1 $operator $operand2 = $result"
-
-    // Shuffle the operators to create wrong answers
-    val wrongOperators = operators.shuffled()
-
-    // Create three wrong problems
-    val wrongProblem1 = "$operand1 ${wrongOperators[0]} $operand2 = ${random.nextInt(20)}"
-    val wrongProblem2 = "$operand1 ${wrongOperators[1]} $operand2 = ${random.nextInt(20)}"
-    val wrongProblem3 = "$operand1 ${wrongOperators[2]} $operand2 = ${random.nextInt(20)}"
-
-    // Randomly select the position for the correct answer
-    val position = random.nextInt(4)
-
-    // Insert the correct problem at the selected position
-    val problems = mutableListOf(wrongProblem1, wrongProblem2, wrongProblem3)
-    problems.add(position, problemWithAnswer)
-
-    // Return the correct problem and the list of problems for UI display
-    return Pair(problemWithAnswer, problems.joinToString(separator = "\n"))
-
-}
-
-fun checkAnswer(problem: String, userAnswer: String): Boolean {
-    try {
-        // Split the problem into operands and operator
-        val (operand1, operator, operand2) = problem.split(" ")
-
-        // Convert operands to integers
-        val num1 = operand1.toInt()
-        val num2 = operand2.toInt()
-
-        // Evaluate the result based on the operator
-        val expectedResult = when (operator) {
-            "+" -> num1 + num2
-            "-" -> num1 - num2
-            "*" -> num1 * num2
-            "/" -> num1 / num2
-            else -> throw IllegalArgumentException("Invalid operator: $operator")
-        }
-
-        // Check if the user's answer is correct
-        return userAnswer.toIntOrNull() == expectedResult
-    } catch (e: Exception) {
-        // Handle any parsing or arithmetic exceptions
-        e.printStackTrace()
-        return false
-    }
-}
 
 
 
--- app/src/main/java/uz/itschool/mathgame/screen/IntroScreen.kt
+++ app/src/main/java/uz/itschool/mathgame/screen/IntroScreen.kt
@@ -1,12 +1,16 @@
 package uz.itschool.mathgame.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
@@ -16,10 +20,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import uz.itschool.mathgame.R
 import uz.itschool.mathgame.navigation.NavGraph
 
 
@@ -29,38 +36,40 @@ fun IntroScreen(navController: NavController){
         modifier = Modifier.fillMaxSize(),
         color = Color.White
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color(0xFF161C44)),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Button(
-                onClick = {
-                          navController.navigate("level_screen")
-                          },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(15.dp),
-            ) {
-                Text(text = "New Game")
-            }
 
-            Button(
-                onClick = {
-                          navController.navigate("record_screen")
-                },
+            Column(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(15.dp),
-                colors = ButtonDefaults.buttonColors()
+                    .fillMaxSize()
+                    .background(Color(0xFF161C44)),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(text = "Record")
+                Button(
+                    onClick = {
+                        navController.navigate("level_screen")
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(20.dp),
+                ) {
+                    Text(text = "New Game")
+                }
+
+                Button(
+                    onClick = {
+                        navController.navigate("record_screen")
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(20.dp),
+                    colors = ButtonDefaults.buttonColors()
+                ) {
+                    Text(text = "Record")
+                }
             }
         }
     }
-}
+
 
 
 @Preview(showBackground = true)
